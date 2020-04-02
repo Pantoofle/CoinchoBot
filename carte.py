5ac992efb4e166a328edc9a2d546417db826f6db
Simon Fernandez <simon.fernandez@ens-lyon.fr>
2020-04-02 18:49:31 +0200

    Recognize Caro as Carreau

--- carte.py
+++ carte.py
@@ -14,6 +14,7 @@ class Color(Enum):
 COLOR_DICT = {
     "Carreau": Color.Carreau,
     "Ca": Color.Carreau,
+    "Carro": Color.Carreau,
     "K": Color.Carreau,
     "Trefle": Color.Trefle,
     "T": Color.Trefle,
